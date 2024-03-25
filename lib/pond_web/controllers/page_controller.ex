65c45f4ad967fd05b0629b40282007a20341e2e7
codehans <94654388+codehans@users.noreply.github.com>
2024-03-25 13:30:12 +0000

    Load latest block

--- lib/pond_web/controllers/page_controller.ex
+++ lib/pond_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
 defmodule PondWeb.PageController do
+  alias Cosmos.Base.Tendermint
   use PondWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    {:ok, %{block: block}} =
+      Tendermint.V1beta1.Service.Stub.get_latest_block(
+        Pond.Node.channel(),
+        Tendermint.V1beta1.GetLatestBlockRequest.new()
+      )
+
+    conn |> assign(:block, block) |> render("index.html")
   end
 end
